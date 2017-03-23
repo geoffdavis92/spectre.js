42f6c1698df183454338094a44116b5615af188b
Geoff Davis <geoffdavis92@gmail.com>
2017-03-22 23:30:41 -0500

    add flowconfig, update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,22 +3,23 @@
 // Prod check/uglify on prodbuild from http://stackoverflow.com/questions/25956937/how-to-build-minified-and-uncompressed-bundle-with-webpack
 
 const PROD = JSON.parse(process.env.PROD_ENV || '0');
+const MIN = JSON.parse(process.env.MIN_ENV || '0');
 const path = require('path');
 const webpack = require('webpack');
 const DashboardPlugin = require('webpack-dashboard/plugin');
 
 module.exports = {
-    entry: './index.js',
+    entry: PROD ? './src/index.js' : './demo.js',
     output: {
     	path: path.join(__dirname,(PROD ? '/dist' : '/dev')),
-    	filename: PROD ? 'spectre.min.js' : 'spectre.js'
+    	filename: MIN ? 'spectre.min.js' : 'spectre.js'
     },
     module: {
     	loaders: [
     		{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
     	]
     },
-    plugins: PROD ? [
+    plugins: MIN ? [
         new webpack.optimize.UglifyJsPlugin({
           compress: { warnings: false }
         })
