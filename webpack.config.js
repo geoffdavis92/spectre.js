d10308e714ea1640b7e3fdb7715ce1528040026b
Geoff Davis <geoffdavis92@gmail.com>
2017-03-22 20:59:04 -0500

    update dev dependencies, webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// Prod check/uglify on prodbuild from http://stackoverflow.com/questions/25956937/how-to-build-minified-and-uncompressed-bundle-with-webpack
+
+const PROD = JSON.parse(process.env.PROD_ENV || '0');
+const path = require('path');
+const webpack = require('webpack');
+const DashboardPlugin = require('webpack-dashboard/plugin');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+    	path: path.join(__dirname,'/dist'),
+    	filename: 'bundle.js'
+    },
+    module: {
+    	loaders: [
+    		{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
+    	]
+    },
+    plugins: PROD ? [
+        new webpack.optimize.UglifyJsPlugin({
+          compress: { warnings: false }
+        })
+    ] : [ new DashboardPlugin() ]
+}
